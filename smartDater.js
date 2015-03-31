9524c09ae4e168e71560b9d5cbac6027e8954875
yinan <lynweklm@gmail.com>
2015-03-31 15:25:50 +0800

    代码结构优化，增加两个动态插入的方法
    Signed-off-by: yinan <lynweklm@gmail.com>

--- smartDater.js
+++ smartDater.js
@@ -1,33 +1,43 @@
 /*******  By Okiilemon  *******/
 /*******  2015.3  *******/
+(function (window, document, undefined) {
 
-var smartDater = {
-    total: 0,
-    allElementNodes: document.getElementsByTagName('*')
+"use strict";
 
-};
+var smartDater = window.smartDater || (window.smartDater = {
+    allElementNodes : [],
+    timeStamps : [],
+    update : undefined,
+    callTheUpdate : undefined,
+    insertNode : undefined,
+    insertNodes : undefined,
+    hasInit: false
+}),
+    that = smartDater,
 
-smartDater.timeStamps = [];
+    selectAllAttributes = function () {
+        if (document.querySelectorAll){
+            that.timeStamps = document.querySelectorAll("[data-timestamp]");
+        }else {
+            smartDater.allElementNodes = document.getElementsByTagName('*');
+            traversal(smartDater.allElementNodes);
+        };
+    },
+    //定义遍历所有节点找到时间戳节点并保存的方法
+    traversal = function (elementNodes) {
 
-//定义遍历所有节点找到时间戳节点并保存的方法
-smartDater.traversal = function (elementNodes) {
-
-    var lenOfElements = elementNodes.length
-    for (var i = 0; i < lenOfElements; i++) {
-        if (elementNodes[i].attributes.length) {
-            var lenOfAttr = elementNodes[i].attributes.length;
-            for (var j = 0; j < lenOfAttr; j++) {
-                if (elementNodes[i].attributes[j].nodeName === 'data-timestamp') {
-                    this.timeStamps[this.total++] = elementNodes[i];
-                }
+        var lenOfElements = elementNodes.length
+        for (var i = 0; i < lenOfElements; i++) {
+            var dataTimestamp = elementNodes[i].getAttribute('data-timestamp');
+            if (dataTimestamp) {
+                that.timeStamps.push(elementNodes[i]);
             }
         }
-    }
-};
+    };
 
 
 //定义刷新时间间隔的方法
-smartDater.update = function (thatTime) {
+that.update = function (thatTime) {
     var currentTime = Date.parse(new Date());
     //得到时间差，单位为'秒'
     var gap = (currentTime - thatTime) / 1000;
@@ -78,23 +88,58 @@ smartDater.update = function (thatTime) {
 };
 
 //定义调用遍历和刷新的方法以更新
-smartDater.callTheUpdate = function () {
+that.callTheUpdate = function () {
 
-    var lenOfTimestamps = this.timeStamps.length;
+    var lenOfTimestamps = that.timeStamps.length,
+        node;
 
     for (var k = 0; k < lenOfTimestamps; k++) {
-        this.timeStamps[k].innerHTML = this.update(this.timeStamps[k].getAttribute('data-timestamp'));
+        node = that.timeStamps[k];
+        node.innerHTML = that.update(node.getAttribute('data-timestamp'));
     }
 
 };
 
+that.insertNode = function (node) {
+    var thatTime;
+    if (node.getAttribute('data-timestamp')) {
+        thatTime = node.getAttribute('data-timestamp');
+        node.innerHTML = that.update(thatTime);
+        console.log(node);
+        that.allElementNodes.push(node);
+    }else {
+        return false;
+    }
+};
+
+that.insertNodes = function (nodes) {
+    var len;
+    if (len = nodes.length) {
+        for (var i = len - 1; i >= 0; i--) {
+            that.insertNode(nodes[i]);
+        };
+    }else {
+        return false;
+    }
+};
+
+that.init = function () {
+    selectAllAttributes();
+    smartDater.callTheUpdate();
+    that.hasInit = true;
+};
+
 //页面载入后立即遍历并更新
 window.onload = function () {
-    smartDater.traversal(smartDater.allElementNodes);
-    smartDater.callTheUpdate();
+    if (!that.hasInit) {
+        that.init();
+    }
 };
 
 //每60s刷新一次
-setInterval(function () {
+window.setInterval(function () {
     smartDater.callTheUpdate()
 }, 60000);
+
+
+})(window, document);
